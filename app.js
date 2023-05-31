3089ff3d63dee626e26212f5137d5b8feb6715b1
Bohan Yang <bohan.k.yang@gmail.com>
2023-05-31 12:40:46 -0700

    Add environment variable

---
+++ app.js
@@ -0,0 +1,88 @@
+//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema(
+    {
+        email: {type: String, required: [true, "Must need an email for username"]},
+        password: {type: String, required: [true, "Must have a password"]}
+    });
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+
+// creating the table, table name is User and using userSchema
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) =>
+{
+    res.render("home");
+});
+
+app.route("/login")
+    .get((req, res) =>
+    {
+        res.render("login");
+    })
+    .post((req, res) =>
+    {
+        const username = req.body.username;
+        const password = req.body.password;
+        User.findOne({email: username}).then(user =>
+        {
+            if (user)
+            {
+                if (user.password === password) res.render("secrets");
+                else res.send("Wrong Password");
+            }
+            else
+                res.send("You have not registered, please check username again");
+
+        }).catch(err =>
+        {
+            res.send(err);
+        });
+
+    });
+
+app.route("/register")
+    .get( (req, res) =>
+    {
+        res.render("register");
+    })
+    .post((req, res) =>
+    {
+        const newUser = new User(
+            {
+                email: req.body.username,
+                password: req.body.password
+            });
+        // when calling save after create a new user, it automatically encrypts password
+        // when we do find(); it automatically decrypts password
+        newUser.save().then(() =>
+        {
+            res.render("home");
+        }).catch(err =>
+        {
+            res.send(err + " 400 Bad request");
+        });
+    });
+
+
+
+app.listen(3000, ()=>
+{
+    console.log("Server started on port 3000");
+});
